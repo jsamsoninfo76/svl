a89369705f721ec7e7510d69e1c59ae32bc7b68c
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@bce8daf3-d774-55bb-16a0-54a916e01c48>
2013-01-21 09:39:45 +0000

    Import c2

---
+++ trunk/tp2/test_bank.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+"""
+Test le module bank.
+"""
+
+import unittest
+
+from bank import Account
+
+
+from mockito import mock
+from mockito import when
+from mockito import verify
+
+
+__date__ = 'Mon Jan 21 09:30:39 2013'
+
+
+class TestJohnOuvreUnCompte(unittest.TestCase):
+    
+    def test_un_nouveau_compte_a_un_solde_de_0(self):
+        """un nouveau compte a un solde de 0"""
+        storage = mock()
+        when(storage).select().thenReturn([])
+        account = Account(storage)
+        balance = account.balance()
+        self.assertEquals(0, balance)
+
+
+class TestJohnConsulteSonCompte(unittest.TestCase):
+
+    def test_le_solde_est_correctement_calcule(self):
+        """le solde est correctement calcule"""
+        storage = mock()
+        when(storage).select().thenReturn([10, -5])
+        account = Account(storage)
+        balance = account.balance()
+        self.assertEquals(5, balance)
+
+
+class TestJohnDebiteSonCompte(unittest.TestCase):
+    
+    def setUp(self):
+        self.storage = mock()
+        self.account = Account(self.storage)
+    
+    def test_debit_ajoute_une_operation_negative_a_l_historique(self):
+        """debit ajoute une operation negative a l historique"""
+        when(self.storage).select().thenReturn([10])
+        amount = 5
+        self.account.debit(amount)
+        verify(self.storage).insert(-amount)
+
+    def test_un_montant_negatif_leve_une_exception(self):
+        """un montant negatif leve une exception"""
+        when(self.storage).select().thenReturn([1])
+        negative_amount = -1
+        self.assertRaises(ValueError, self.account.debit, negative_amount)
+
+    def test_debit_avec_montant_superieur_au_solde_leve_une_exception(self):
+        """debit avec montant superieur au solde leve une exception"""
+        when(self.storage).select().thenReturn([5])
+        too_big_amount = 10
+        self.assertRaises(ValueError, self.account.debit, too_big_amount)
+
+
+class TestJohnCrediteSonCompte(unittest.TestCase):
+
+    def setUp(self):
+        self.storage = mock()
+        self.account = Account(self.storage)
+    
+    def test_credit_ajoute_une_operation_a_l_historique(self):
+        """credit ajoute une operation a l historique"""
+        amount = 10
+        self.account.credit(amount)
+        verify(self.storage).insert(amount)
+
+    def test_un_montant_negatif_leve_une_exception(self):
+        """un montant negatif leve une exception"""
+        montant_negatif = -1
+        self.assertRaises(ValueError, self.account.credit, montant_negatif)
+
+# eof
